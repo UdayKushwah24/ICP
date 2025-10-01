613a2bfc3edd177c746936c49982446da124e332
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-10-01 23:00:51 +0530

    Daily Practice

---
+++ DailyLeetcodeQuestion/MinTimeCompleteTrips_2187.java
@@ -0,0 +1,14 @@
+package InfinityChampionsProgram.DailyLeetcodeQuestion;
+
+public class MinTimeCompleteTrips_2187 {
+
+
+    public static long minimumTime(int[] time, int totalTrips) {
+
+    }
+    public static void main(String[] args) {
+        int[] time = { 1, 2, 3 };
+        int totalTrips = 5;
+        System.out.println(minimumTime(time, totalTrips));
+    }
+}
